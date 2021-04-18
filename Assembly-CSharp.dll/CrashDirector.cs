be17bc358ede5eb9fab6ff1e3653ce70ae5c494b
MSMMODDER9548001 <MSMMODDER9548001@sharklasers.com>
2021-04-18 22:12:44 +0200

    MSM-06: max 10 chrashes each race + cleanup unused code

--- Assembly-CSharp.dll/CrashDirector.cs
+++ Assembly-CSharp.dll/CrashDirector.cs
@@ -26,85 +26,18 @@ public class CrashDirector
 			this.mRealSafetyCarCount = 2;
 			break;
 		}
+
 		this.mSessionManager = Game.instance.sessionManager;
 		this.mSafetyCar = Game.instance.vehicleManager.safetyVehicle;
-		float random = RandomUtility.GetRandom01();
-		int i;
-		if (random > 0.99f)
-		{
-			i = 8;
-		}
-		else if (random > 0.97f)
-		{
-			i = 7;
-		}
-		else if (random > 0.9f)
-		{
-			i = 6;
-		}
-		else if (random > 0.8f)
-		{
-			i = 5;
-		}
-		else if (random > 0.6f)
-		{
-			i = 4;
-		}
-		else if (random > 0.5f)
-		{
-			i = 3;
-		}
-		else if (random > 0.4f)
-		{
-			i = 2;
-		}
-		else if (random > 0.2f)
-		{
-			i = 1;
-		}
-		else
-		{
-			i = 0;
-		}
-		if (this.mSessionManager.currentSessionWeather.GetAverageWeather().rainType >= Weather.RainType.Heavy && i < 4)
-		{
-			i += RandomUtility.GetRandom(1, 2);
-		}
-		this.mCrashChunks = new CrashDirector.CrashRaceChunk[10];
-		for (int j = 0; j < this.mCrashChunks.Length; j++)
-		{
-			this.mCrashChunks[j] = new CrashDirector.CrashRaceChunk();
-			this.mCrashChunks[j].normalizedChunkStart = (float)j / ((float)this.mCrashChunks.Length - 1f);
-			this.mCrashChunks[j].normalizedChunkSize = 1f / ((float)this.mCrashChunks.Length - 1f);
-		}
-		global::Debug.LogFormat("Crashes for this race: {0} ", new object[]
-		{
-			i
-		});
-		while (i > 0)
-		{
-			i--;
-			this.mCrashChunks[RandomUtility.GetRandom(0, this.mCrashChunks.Length)].crashCount++;
-		}
-		this.mVehiclesCantCrash.Clear();
 	}
 
 	public void OnLoad()
 	{
-		if (this.mCrashChunks.Length == 0)
-		{
-			this.OnSessionStarting();
-		}
-		if (this.mVehiclesCantCrash == null)
-		{
-			this.mVehiclesCantCrash = new List<RacingVehicle>();
-		}
 	}
 
 	public void OnSessionEnd()
 	{
 		this.mSafetyCar = null;
-		this.mVehiclesCantCrash.Clear();
 	}
 
 	public void OnCrashIncident(RacingVehicle inVehicle)
@@ -157,14 +90,6 @@ public class CrashDirector
 
 	public void SimulationUpdate()
 	{
-		for (int i = 0; i < this.mCrashChunks.Length; i++)
-		{
-			if (this.mCrashChunks[i].IsActiveChunk(Game.instance.sessionManager.GetNormalizedSessionTime()))
-			{
-				this.mActiveChunk = this.mCrashChunks[i];
-				break;
-			}
-		}
 		if (this.mSessionManager.flag == SessionManager.Flag.VirtualSafetyCar)
 		{
 			this.mVirtualSafetyFlagDuration -= GameTimer.simulationDeltaTime;
@@ -201,35 +126,15 @@ public class CrashDirector
 		bool isTutorialActiveInCurrentGameState = Game.instance.tutorialSystem.isTutorialActiveInCurrentGameState;
 		bool isRace = sessionManager.sessionType == SessionDetails.SessionType.Race;
 		bool noRaceStartEnd = sessionManager.lap > 1 && sessionManager.lapCount - sessionManager.lap >= 3;
-		// bool flag4 = this.mActiveChunk.crashCount > 0;
-		bool crashesLeft = mCrashVehicle.Count < 10;
+		bool crashesLeft = mCrashVehicle.Count < CrashDirector.MAX_CRASHES;
 		bool isGreenFlag = Game.instance.sessionManager.flag == SessionManager.Flag.Green;
-		return isGreenFlag && !isTutorialActiveInCurrentGameState && safetyCarReady && isRace && noRaceStartEnd
-		&& !inVehicle.behaviourManager.isOutOfRace && (this.mVehiclesCantCrash == null || !this.mVehiclesCantCrash.Contains(inVehicle))
-		&& inVehicle.sessionEvents.IsReadyTo(SessionEvents.EventType.Crash);
+		return isGreenFlag && crashesLeft && !isTutorialActiveInCurrentGameState && safetyCarReady && isRace && noRaceStartEnd
+		&& !inVehicle.behaviourManager.isOutOfRace && inVehicle.sessionEvents.IsReadyTo(SessionEvents.EventType.Crash);
 	}
 
 	public static RacingVehicle GetTeamMate(RacingVehicle inVehicle)
 	{
 		return Game.instance.vehicleManager.GetVehicleTeamMate(inVehicle);
-	}
-
-	public void AddCrash()
-	{
-		this.mActiveChunk.crashCount++;
-	}
-
-	public void AddVehicleCantCrash(RacingVehicle inVehicle)
-	{
-		if (!this.mVehiclesCantCrash.Contains(inVehicle))
-		{
-			this.mVehiclesCantCrash.Add(inVehicle);
-		}
-	}
-
-	public void RemoveVehicleCantCrash(RacingVehicle inVehicle)
-	{
-		this.mVehiclesCantCrash.Remove(inVehicle);
 	}
 
 	public float virtualSafetyCarTimer
@@ -248,38 +153,16 @@ public class CrashDirector
 		}
 	}
 
-	private List<RacingVehicle> mVehiclesCantCrash = new List<RacingVehicle>();
-
 	private int mRealSafetyCarCount = 1;
 
 	private float mVirtualSafetyFlagDuration;
-
-	private CrashDirector.CrashRaceChunk[] mCrashChunks = new CrashDirector.CrashRaceChunk[0];
-
-	private CrashDirector.CrashRaceChunk mActiveChunk;
 
 	private SafetyVehicle mSafetyCar;
 
 	private SessionManager mSessionManager;
 	
 	private List<RacingVehicle> mCrashVehicle = new List<RacingVehicle>();
+	
+	private static int MAX_CRASHES = 10;
 
-	[fsObject(MemberSerialization = fsMemberSerialization.OptOut)]
-	private class CrashRaceChunk
-	{
-		public CrashRaceChunk()
-		{
-		}
-
-		public bool IsActiveChunk(float inNormalizedRaceTime)
-		{
-			return inNormalizedRaceTime >= this.normalizedChunkStart && inNormalizedRaceTime < this.normalizedChunkStart + this.normalizedChunkSize;
-		}
-
-		public float normalizedChunkSize;
-
-		public float normalizedChunkStart;
-
-		public int crashCount;
 	}
-}
