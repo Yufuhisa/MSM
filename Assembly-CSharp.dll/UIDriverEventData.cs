b0bb91669900d2864d15156ea8fe3ef7a2a125b6
MSMMODDER9548001 <MSMMODDER9548001@sharklasers.com>
2021-05-16 20:54:18 +0200

    MSM-14: Add DSQ (disqualified) in Overview for Championship

--- Assembly-CSharp.dll/UIDriverEventData.cs
+++ Assembly-CSharp.dll/UIDriverEventData.cs
@@ -17,6 +17,7 @@ public class UIDriverEventData : MonoBehaviour
 			int points = -1;
 			int position = 20;
 			bool dnfFlag = false;
+			bool dsqFlag = false;
 			int count = inDetails.results.GetAllResultsForSession(SessionDetails.SessionType.Race).Count;
 			for (int i = 0; i < count; i++)
 			{
@@ -31,25 +32,48 @@ public class UIDriverEventData : MonoBehaviour
 					position = resultForDriver.position;
 					if (resultForDriver.carState != RaceEventResults.ResultData.CarState.None)
 						dnfFlag = true;
+					
+					foreach (Penalty penalaty in resultForDriver.penalties) {
+						if (penalaty is PenaltyPartRulesBroken) {
+							dsqFlag = true;
+						}
+					}
+					
+					
 				}
 			}
 			
-			if (dnfFlag && points <= 0)
+			if (dsqFlag) {
+				this.pointsForEvent.text = "DSQ";
+				this.pointsForEvent.color = UIDriverEventData.COLOR_DSQ;
+				this.backing.enabled = false;
+			}
+			else if (dnfFlag && points <= 0) {
 				// if retired or crashed and didnt get any points -> show DNF for this race
 				this.pointsForEvent.text = "DNF";
-			else
-				this.pointsForEvent.text = ((points != -1) ? ((points != 0) ? points.ToString() : string.Empty) : "-");
-			
-			this.pointsForEvent.color = ((points > 0) ? this.labelColor : this.labelColorFaded);
-			if (position >= 1 && position <= 3)
-			{
-				position = Mathf.Clamp(position - 1, 0, this.podiumColors.Length);
-				this.backing.enabled = true;
-				this.backing.color = this.podiumColors[position];
-			}
-			else
-			{
+				this.pointsForEvent.color = this.labelColorFaded;
 				this.backing.enabled = false;
+			}
+			else if (points == -1) {
+				this.pointsForEvent.text = "-";
+				this.pointsForEvent.color = this.labelColorFaded;
+				this.backing.enabled = false;
+			}
+			else if (points == 0) {
+				this.pointsForEvent.text = string.Empty;
+				this.pointsForEvent.color = this.labelColorFaded;
+				this.backing.enabled = false;
+			}
+			else {
+				this.pointsForEvent.text = points.ToString();
+				this.pointsForEvent.color = this.labelColor;
+				this.backing.enabled = false;
+				
+				if (position >= 1 && position <= 3) {
+					this.backing.enabled = true;
+					position = Mathf.Clamp(position - 1, 0, this.podiumColors.Length);
+					this.backing.color = this.podiumColors[position];
+				}
 			}
 		}
 	}
@@ -63,4 +87,6 @@ public class UIDriverEventData : MonoBehaviour
 	public Color labelColor = default(Color);
 
 	public Color labelColorFaded = default(Color);
+	
+	private static Color COLOR_DSQ = new Color(255f, 0f, 0f);
 }
