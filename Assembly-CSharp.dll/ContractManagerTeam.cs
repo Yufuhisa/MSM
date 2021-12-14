54eee9d04b0ba066321dfc452a3b5c0481edf831
Yufuhisa <95189280+Yufuhisa@users.noreply.github.com>
2021-12-14 21:50:27 +0100

    Revert "Original"
    This reverts commit 7b7a49e2b1659c333824ee31de99a340c36ff0a8.

--- Assembly-CSharp.dll/ContractManagerTeam.cs
+++ Assembly-CSharp.dll/ContractManagerTeam.cs
@@ -884,7 +884,6 @@ public class ContractManagerTeam
 
 	public void ReplaceCurrentDriverWithNewOne(ContractPerson inDraftContract, Person inNewPersonToHire, Person inPersonToReplace)
 	{
-		int newID = ((Driver)inNewPersonToHire).carID;
 		int oldID = ((Driver)inPersonToReplace).carID;
 		StringVariableParser.personReplaced = inPersonToReplace;
 		inDraftContract.SetCurrentStatus(inPersonToReplace.contract.currentStatus);
@@ -896,20 +895,20 @@ public class ContractManagerTeam
 		{
 			driver.carOpinion.CalculateDriverOpinions(driver);
 		}
-		if (this.mTeam.championship.series != Championship.Series.EnduranceSeries) {
-			driver.SetCarID(oldID);
-		}
-/*
-		global::Debug.LogErrorFormat("ReplaceCurrentDriverWithNewOne - Old driver {0} ID {1}->{2} with new Driver {3} ID {4}->{5}", new object[]
+		driver.SetCarID(oldID);
+		if (this.mTeam.IsPlayersTeam())
 		{
-			inPersonToReplace.shortName,
-			oldID,
-			((Driver)inPersonToReplace).carID,
-			inNewPersonToHire.shortName,
-			newID,
-			((Driver)inNewPersonToHire).carID
-		});
-*/
+			global::Debug.LogErrorFormat("ReplaceCurrentDriverWithNewOne - Player Team Driver 1 is {0} and has CarID {1}", new object[]
+			{
+				this.mTeam.GetDriver(0).shortName,
+				this.mTeam.GetDriver(0).carID
+			});
+			global::Debug.LogErrorFormat("ReplaceCurrentDriverWithNewOne - Player Team Driver 2 is {0} and has CarID {1}", new object[]
+			{
+				this.mTeam.GetDriver(1).shortName,
+				this.mTeam.GetDriver(1).carID
+			});
+		}
 	}
 
 	public void ReplacePersonWithNewOne(ContractPerson inDraftContract, Person inNewPersonToHire, Person inPersonToReplace)
